6a47e38250d87539d66bd94c5f61bfe3f3fbd202
bhattarai <bhattarai.utk1@gmail.com>
2019-12-03 22:59:33 -0600

    Making two fragments

--- app/src/main/java/com/example/fragmentation/MainActivity.java
+++ app/src/main/java/com/example/fragmentation/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.fragmentation;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
+    private Button btnFragment;
+    private Boolean status=true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnFragment=findViewById(R.id.btnFragment);
+        btnFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager=getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+                if (status)
+                {
+                    firstFragment firstFragment=new
+                }
+
+            }
+        });
     }
 }
